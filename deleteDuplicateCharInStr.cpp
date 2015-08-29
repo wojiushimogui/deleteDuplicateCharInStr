53b6572d915c337b990dd74e816a15451c9d0308
wuranghao <154943046@qq.com>
2015-08-29 12:44:09 +0800

    delete duplicate char in String

---
+++ deleteDuplicateCharInStr.cpp
@@ -0,0 +1,21 @@
+/*
+移除字符串中重复的字符，
+如abcadc移除后变为abcd，
+可以额外定义一两个变量，但不允许额外开辟一个数组。
+*/
+
+#include<stdio.h>
+#include<string.h>
+
+void deleteDuplicateCharInStr(char *str){
+	
+	
+}
+
+int main(void){
+	char str[1000];
+	gets(str);
+	deleteDuplicateCharInStr(str);
+	return 0;
+}
+
